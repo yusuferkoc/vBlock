b1ffdfcbb501106a111b27cbd90bf035507f1863
yusuf erkoc <yusuferkoc99@gmail.com>
2022-06-16 21:54:43 +0300

    for deploy

---
+++ client/src/components/Transactions.jsx
@@ -0,0 +1,15 @@
+const Transactions = () => {
+    return (
+        <div className="flex w-full justify-center items-center 2x1:px-20 gradient-bg-transactions">
+            <div className="flex flex-col md:p-12 py-12 px-4">
+            <h1 className='text-white text-xl sm:text-xl py-2 text-gradient'>
+                        KTUN'de gerçekleşen  
+                        <br />
+                        Son Transaction
+                    </h1>
+            </div>
+        </div>
+    );
+}
+
+export default Transactions;
---
+++ client/src/utils/constants.js
@@ -0,0 +1,5 @@
+import abi from './Transactions.json'
+
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x9E49F5d3BA68217012dE52fA73AB9015e004727c';
